e3953f2af5efcedc0e45f5ea4083a8ef986d54ab
Ayaaa <aya_japan201@yahoo.co.jp>
2021-03-22 16:34:30 -0700

    security Level 1

---
+++ app.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.json()); //Used to parse JSON bodies
+app.use(express.urlencoded({extended: true})); //Parse URL-encoded bodies
+
+mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const userSchema = {
+    email: String,
+    password: String
+};
+const User = mongoose.model("User", userSchema);
+
+app.get("/", (req,res)=>{
+    res.render("home")
+});
+
+app.route("/login")
+    .get((req,res)=>{
+    res.render("login")
+})
+    .post((req,res)=>{
+        User.findOne({email: req.body.username},(err, result)=>{
+            if(err){
+                console.log(err);
+            }else{
+                if(result){
+                    if(result.password === req.body.password){
+                        res.render("secrets")
+                    }
+                }
+            }
+        })
+    })
+
+app.route("/register")
+    .get((req,res)=>{
+        res.render("register")
+    })
+    .post((req,res)=>{
+        const newUser = new User({
+            email: req.body.username,
+            password: req.body.password
+        });
+        newUser.save((err)=>{
+            if(!err){
+                res.render("secrets")
+            }else{
+                console.log(err);
+            }    
+        });
+   });
+
+
+
+
+
+app.listen(3000, ()=>{
+    console.log("server started on port 3000");
+});
