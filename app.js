fe893a303547b12d52968ce63c3d05fb627da738
Ayaaa <aya_japan201@yahoo.co.jp>
2021-03-26 18:06:43 -0700

    Edit submit page

--- app.js
+++ app.js
@@ -38,7 +38,8 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String,
     googleId: String,
-    facebookId: String
+    facebookId: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -159,12 +160,41 @@ app.route("/register")
     });
 
 app.get("/secrets", (req, res) => {
-    if (req.isAuthenticated()) {
-        res.render("secrets")
-    } else {
-        res.redirect("/login")
-    }
+    User.find({secret: {$ne: null}}, (err, foundUsers)=>{
+        if(err){
+            console.log(err);
+        }else{
+            if(foundUsers){
+                res.render("secrets", {foundUsers: foundUsers})
+            }
+        }
+    })
 });
+
+app.route("/submit")
+    .get((req, res) => {
+        if (req.isAuthenticated()) {
+            res.render("submit")
+        } else {
+            res.redirect("/login")
+        }
+    })
+    .post((req, res)=>{
+        User.findById(req.user.id, (err, foundUser)=>{
+            if(err){
+                console.log(err);
+            }else{
+                if(foundUser){
+                    foundUser.secret = req.body.secret;
+                    foundUser.save(()=>{
+                        res.redirect("/secrets")
+                    })
+                }
+            }
+        })
+    })
+
+
 
 app.get("/logout", (req, res) => {
     req.logout();
